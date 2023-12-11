653c29ec60c49c900e05cfd2054afd5d334880f3
Aziz <aziz.saydaminov02@gmail.com>
2023-12-10 23:58:17 -0600

    Fixes some build bugs.

--- Assets/Scripts/Tutorial/Scripts/TutorialAgent.cs
+++ Assets/Scripts/Tutorial/Scripts/TutorialAgent.cs
@@ -21,7 +21,6 @@ public class TutorialAgent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentTrigger = -1;
     }
 
     // Update is called once per frame
